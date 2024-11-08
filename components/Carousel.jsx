d17b23eecf1fa1ca52607e0de49f264ca17f30a8
Ahmet Dursun <82242698+ahm3taksk@users.noreply.github.com>
2024-11-09 02:31:55 +0300

    Fixed minor bug on slider paging

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -14,11 +14,11 @@ const Carousel = () => {
     autoplaySpeed: 30000,
     appendDots: (dots) => (
         <div>
-            <ul className="container mx-auto w-full text-start">{dots}</ul>
+            <ul className="container mx-auto px-5 sm:px-0 w-full text-start">{dots}</ul>
         </div>
     ),
     customPaging: (i) => (
-        <div className="w-3 h-3 border bg-white rounded-full mt-10"></div>
+        <div className="w-3 h-3 border bg-white rounded-full"></div>
     ),
   };
 
@@ -35,9 +35,9 @@ const Carousel = () => {
                 />
             </div>
         </div>
-        <Slider {...settings} className="px-5 sm:px-0">
+        <Slider {...settings} >
             <div>
-            <div className="mt-48 container mx-auto  text-white flex flex-col items-start gap-y-8">
+            <div className="mt-48 container mx-auto px-5 sm:px-0 text-white flex flex-col items-start gap-y-8">
                 <Title addClass="text-5xl">Fast Food Restaurant</Title>
                 <p className="text-sm sm:w-2/5 w-full">
                 Doloremque, itaque aperiam facilis rerum, commodi, temporibus
@@ -49,7 +49,7 @@ const Carousel = () => {
             </div>
             </div>
             <div>
-                <div className="mt-48 container mx-auto  text-white flex flex-col items-start gap-y-8">
+                <div className="mt-48 container mx-auto px-5 sm:px-0 text-white flex flex-col items-start gap-y-8">
                     <Title addClass="text-5xl">Adana Kebapcine</Title>
                     <p className="text-sm sm:w-2/5 w-full">
                     Doloremque, itaque aperiam facilis rerum, commodi, temporibus
